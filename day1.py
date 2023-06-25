5c2345eb0b0b36d86b65c9a276c333107d334409
kanikasawant <137372086+kanikasawant@users.noreply.github.com>
2023-06-25 20:53:22 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,16 @@
+#print("Kanika")
+#num=10 #int
+#print(type(num))
+#num="10" #str
+#print(type(num))
+
+
+#num=int(input("Enter a number"))
+#print("The number is",num)
+
+
+a=int(input ("enter a number"))
+b=int(input ("enter a number"))
+c=int(input("enter a number"))
+d= (a+b+c) /3 
+print (d)
